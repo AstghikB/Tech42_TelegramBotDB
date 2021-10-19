165e53977f7d57cd026acfa462fafbf47477d4a6
Astghik <Astghikburnuchyan@gmail.com>
2021-10-19 23:36:40 +0400

    addIntoRrpository

---
+++ CodeFirst/Models/Bank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Models
+{
+    class Bank
+    {
+        public int ID { get; set; }
+        public string BankName { get; set; }
+        public string BankURL { get; set; }
+    }
+}
